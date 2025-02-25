2a11e1f70d78645922b8346a2ddea4bc039b1b2d
wjd4204 <brian876373@gmail.com>
2025-02-25 16:33:10 +0900

    Section 4. 자동으로 수행되는 테스트

--- src/main/java/tobyspring/hellospring/Client.java
+++ src/main/java/tobyspring/hellospring/Client.java
@@ -15,17 +15,7 @@ public class Client {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
         PaymentService paymentService = beanFactory.getBean(PaymentService.class);
 
-        Payment payment1 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment1: " + payment1);
-        System.out.println("---------------------------------------");
-
-        Payment payment2 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment2: " + payment2);
-        System.out.println("---------------------------------------");
-
-        TimeUnit.SECONDS.sleep(3);
-
-        Payment payment3 = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println("Payment3: " + payment3);
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println("Payment: " + payment);
     }
 }
--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -12,13 +12,13 @@ public class ObjectFactory {
 
     @Bean
     public PaymentService paymentService() {
-        return new PaymentService(cachedExRateProvider());
+        return new PaymentService(exRateProvider());
     }
 
-    @Bean
-    public ExRateProvider cachedExRateProvider() {
-        return new CachedExRateProvider(exRateProvider());
-    }
+//    @Bean
+//    public ExRateProvider cachedExRateProvider() {
+//        return new CachedExRateProvider(exRateProvider());
+//    }
 
     @Bean
     public ExRateProvider exRateProvider(){
