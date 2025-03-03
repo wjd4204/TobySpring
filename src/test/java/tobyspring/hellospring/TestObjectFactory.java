cc6c38964e6c6d8fb944d42fa3c889e6162badb4
wjd4204 <brian876373@gmail.com>
2025-03-03 19:06:05 +0900

    Section 4. 테스트와 DI

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,29 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import static java.math.BigDecimal.valueOf;
+
+@Configuration
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+//    @Bean
+//    public ExRateProvider cachedExRateProvider() {
+//        return new CachedExRateProvider(exRateProvider());
+//    }
+
+    @Bean
+    public ExRateProvider exRateProvider(){
+        return new ExRateProviderStub(valueOf(1_000));
+    }
+}
--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,23 +17,25 @@ class PaymentServiceTest {
 
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증한다.")
     @Test
-    void prepare() throws IOException {
+    void createAccount() throws IOException {
      //given
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
 
-     //when
+        // 원화 환상 금액 유효시간 계산하기
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+
+    }
+
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
-     //then
-        // 환율정보 가져오기
-        assertThat(payment.getExRate()).isNotNull();
-        // 원화 환산 금액 계산하기
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
         assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-        // 원화 환상 금액 유효시간 계산하기
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
-
+                .isEqualByComparingTo(convertedAmount);
     }
 
 }
